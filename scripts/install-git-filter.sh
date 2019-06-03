72c8f9f3179c143c896b2197fda4c2f92c2f9bfc
hoijui <hoijui.quaero@gmail.com>
2019-06-03 09:49:37 +0200

    install-git-filter.sh: Try to get the latest filter JAR version

--- scripts/install-git-filter.sh
+++ scripts/install-git-filter.sh
@@ -111,7 +111,8 @@ else
 		rm ${binary_file_glob}
 
 		target_dir="${this_script_dir}/../target"
-		local_binary=`find "$target_dir" -maxdepth 1 -type f -name "rezipdoc-*.jar"  | grep -v "\-sources" | grep -v "\-javadoc" | head -1`
+		# Try to get the latest version, in case there are multiple ones
+		local_binary=`find "$target_dir" -maxdepth 1 -type f -name "rezipdoc-*.jar" | grep -v "\-sources" | grep -v "\-javadoc" | sort --version-sort | tail -1`
 		if [ "$use_local_binary_if_available" = "true" -a "local_binary" != "" ]
 		then
 			cp "$local_binary" ./
