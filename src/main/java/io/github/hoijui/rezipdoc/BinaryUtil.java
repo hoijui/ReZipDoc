c88c10423198d751ccc014ebe2e168faf443c06c
hoijui <hoijui.quaero@gmail.com>
2020-03-22 17:01:25 +0100

    remove unused imports [nlc]

--- src/main/java/io/github/hoijui/rezipdoc/BinaryUtil.java
+++ src/main/java/io/github/hoijui/rezipdoc/BinaryUtil.java
@@ -26,8 +26,6 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import java.util.stream.Stream;
 
 /**
